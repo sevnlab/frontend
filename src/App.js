c112ebd7a7dd07cc428f1898af89af25e697b3c9
twkim-1 <x_o_d_n@naver.com>
2023-09-27 14:39:17 +0900

    스프링부트 7777 proxy 설정 axios 설치

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import './App.css';
-import SignIn from './components/signIn';
-import SignUp from './components/signUp';
+
 
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from "./App";
 import './index.css';
-// import reportWebVitals from './reportWebVitals';
 import SignIn from "./components/signIn";
 import SignUp from "./components/signUp";
 
@@ -18,7 +17,3 @@ render(
   </Router>, rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
