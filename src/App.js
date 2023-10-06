fecd4cf57589d19d806aea3ab793e60210d2228d
twkim-1 <x_o_d_n@naver.com>
2023-10-06 17:54:50 +0900

    백엔드연결

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './App.css';
-
+import {useEffect, useState} from "react";
 
 
 function App() {
-  return (
-      <div>
-          <h1>MainPage</h1>
-          <nav>
-              {/*Link 태그의 to 속성은 index.js에서 설정해둔 url의 component가 보여지게 한다.*/}
-              {/*to 속성에 index.js에서 설정한 path값을 넣어주면 됨*/}
-              <Link to="/signIn">로그인</Link> |{" "}
-              <Link to="/signUp">회원가입</Link>
-          </nav>
-      </div>
-  );
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("/")
+            .then((res) => {
+                return res.json();
+            })
+            .then(function (result) {
+                setData(result);
+            })
+    }, [])
+
+    return (
+        <div>
+            <h1>MainPage</h1>
+            <nav>
+                {/*Link 태그의 to 속성은 index.js에서 설정해둔 url의 component가 보여지게 한다.*/}
+                {/*to 속성에 index.js에서 설정한 path값을 넣어주면 됨*/}
+                <Link to="/signIn">로그인</Link> |{" "}
+                <Link to="/signUp">회원가입</Link>
+            </nav>
+            <ul>
+                {data.map((v,idx)=><li key={`${idx}-${v}`}>{v}</li>)}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
